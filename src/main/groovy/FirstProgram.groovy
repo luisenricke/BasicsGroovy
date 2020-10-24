36d040d8d0f4aef71dba88ef05e4bcb6ef050c0b
Luis Villalobos <luisevm24@outlook.com>
2020-10-24 13:49:58 -0500

    update file with comments and notes

--- src/main/groovy/FirstProgram.groovy
+++ src/main/groovy/FirstProgram.groovy
@@ -1,7 +1,18 @@
+/*
+    Groovy import by default many libs
+ */
+
 class FirstProgram {
 
     static void main(String[] args) {
+        // First comment in line
         println("Hello world")
+
+        println "There is other way to print line "
+        print("You can avoid semicolons");
+        /*
+        End of the program
+         */
     }
 
 }
